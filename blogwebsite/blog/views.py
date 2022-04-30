35266e44188f2f49a9a52c8ec5b34619608683a7
Mi-As <m.astor@gmx.de>
2022-04-30 16:26:35 +0200

    PostView restructured

--- blogwebsite/blog/views.py
+++ blogwebsite/blog/views.py
@@ -1,25 +1,30 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, redirect
+from django.views import generic, View
+
 from .models import Post, Comment
+from .forms import CommentForm
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
     template_name = 'blog/post_list.html'
 
 
-class PostDetail(generic.DetailView):
-    model = Post
+class PostView(View):
 
-    def get_template_names(self):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(slug=kwargs.get("slug"))
 
-        context = self.get_context_data()
+        if (post_data.status == 1 or self.request.user.is_superuser):
+            context = {}
+            context["post"] = post_data
+            context["comment_list"] = \
+                Comment.objects.filter(post_slug=post_data.slug).values()
+            return render(request,'blog/post_detail.html', context)
 
-        if (context["object"].status == 1 or self.request.user.is_superuser):
-            return'blog/post_detail.html'
+        return render(request,'page/404_page.html')
 
-        return 'page/404_page.html'
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(request.POST)
+        # if form.is_valid():
+        return redirect(f'/blog/{kwargs.get("slug")}#comments')
 
-    def get_context_data(self, **kwargs):
-        context = super(PostDetail, self).get_context_data(**kwargs)
-        context["comment_list"] = Comment.objects.filter(post_slug=context["object"].slug).values()
-        return context
